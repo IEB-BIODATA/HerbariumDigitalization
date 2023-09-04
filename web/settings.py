9676b688509fa8c259d378c5029a5738b14e3ba0
Juan Saez Hidalgo <juan.saez.hidalgo@gmail.com>
2023-09-04 12:34:33 -0300

    Adding csrf trusted origins

--- web/settings.py
+++ web/settings.py
@@ -202,6 +202,8 @@ else:
         'https://www.herbariodigital.cl',
     )
 
+CSRF_TRUSTED_ORIGINS = ['https://*.herbariodigital.cl']
+
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
