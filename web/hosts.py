65bf10d59b9c28b32540545e3eea33b3c6148ac4
Juan Saez Hidalgo <juan.saez.hidalgo@gmail.com>
2023-10-10 09:59:28 -0300

    Patch solution for redirecting to digitalization and api hosts

--- web/hosts.py
+++ web/hosts.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django_hosts import patterns, host
 
 host_patterns = patterns(
@@ -5,3 +6,9 @@ host_patterns = patterns(
     host(r'digitalizacion', 'web.main_urls', name='digitalization'),
     host(r'api', 'web.api_urls', name='api'),
 )
+
+if settings.DEBUG:
+    host_patterns += patterns(
+        '',
+        host(r'localhost', 'web.main_urls', name='localhost'),
+    )
