1be7c97929d35ab487de5a4eee7c18a0dc3d9024
Juan Saez Hidalgo <juan.saez.hidalgo@gmail.com>
2024-01-04 17:38:06 -0300

    Add lost state

--- apps/digitalization/views.py
+++ apps/digitalization/views.py
@@ -848,7 +848,7 @@ def vouchers_download(request):
         ]
         logging.debug("Filtering voucher according to state")
         species = VouchersView.objects.values_list(*headers).filter(
-            Q(voucher_state=1) | Q(voucher_state=7) | Q(voucher_state=8)
+            Q(voucher_state=1) | Q(voucher_state=3) | Q(voucher_state=4) | Q(voucher_state=7) | Q(voucher_state=8)
         ).order_by('id')
         databook = tablib.Databook()
         data_set = tablib.Dataset(*species, headers=headers, title='Vouchers')
