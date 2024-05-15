81c6f0f16213a10753e7a0067c1a720a60b08c0a
Juan Saez Hidalgo <jmsaez@ieb-chile.cl>
2024-05-15 13:09:45 -0400

    Add all herbarium to home view

--- intranet/main_urls.py
+++ intranet/main_urls.py
@@ -36,4 +36,4 @@ handler500 = 'intranet.views.handler500'
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static('media/', document_root=settings.MEDIA_ROOT)
